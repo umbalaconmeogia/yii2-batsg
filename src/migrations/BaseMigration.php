c633c386c93051184c98ede63366ac8174938805
Tran Trung Thanh <umbalaconmeogia@gmail.com>
2018-07-20 02:46:30 +0900

    Add default option when create table in BaseMigration

--- src/migrations/BaseMigration.php
+++ src/migrations/BaseMigration.php
@@ -125,6 +125,11 @@ class BaseMigration extends Migration
             }
 
             // Create table.
+            if ($options === NULL && $this->db->driverName === 'mysql') {
+                // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+                $options = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            }
+
             $this->createTable($table, $definitions, $options);
             $tableCreated = TRUE;
 
