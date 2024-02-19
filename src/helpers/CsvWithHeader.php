7ea0b14524f81ed7c2f42552478a562b825dcc62
Tran Trung Thanh <umbalaconmeogia.gmail>
2024-02-19 10:44:03 +0900

    Move HDateTime to php-util.

--- src/helpers/CsvWithHeader.php
+++ src/helpers/CsvWithHeader.php
@@ -2,8 +2,37 @@
 namespace batsg\helpers;
 
 /**
+ * This class help dealing with CSV file that has the first row as data header.
+ *
+ * By using CsvWithHeader, we can access to CSV element data via col name defined in header row.
+ *
+ * Example of usage:
+ * ```php
+ *   CsvWithHeader::read('employee.csv', function($csv) {
+ *       while ($csv->loadRow() !== FALSE) {
+ *           // Get attributes as an array.
+ *           $attr = $csv->getRowAsAttributes();
+ *           // Display "name" attribute of data row.
+ *           echo 'Employee name: ' . $attr['name'] . "\n";
+ *       }
+ *   });
+ * ```
+ *
+ * If you have CSV file content in memory (for example, file content is post from form), use CsvWithHeader as below.
+ * ```php
+ *   $stream = fopen('php://memory', 'r+');
+ *   fwrite($stream, $csvText);
+ *   rewind($stream);
+ *   CsvWithHeader::read($stream, function($csv) {
+ *       while ($csv->loadRow() !== FALSE) {
+ *           // Get attributes as an array.
+ *           $attr = $csv->getRowAsAttributes();
+ *           // Display "name" attribute of data row.
+ *           echo 'Employee name: ' . $attr['name'] . "\n";
+ *       }
+ *   });
+ * ```
+ * @author thanh
  * @deprecated Use \umbalaconmeogia\phputil\data\CsvWithHeader instead.
  */
-class CsvWithHeader extends \umbalaconmeogia\phputil\data\CsvWithHeader
-{
-}
+class CsvWithHeader extends \umbalaconmeogia\phputil\data\CsvWithHeader {}
